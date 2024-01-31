4f436bbe74222bffd9c7f96eec085f042f8a0c13
AkitaKen Finance <157900487+Akitaswapfi@users.noreply.github.com>
2024-01-31 17:06:56 +0530

    Update Router.sol
    Added Ownership on Rebase Yield claim

--- Router.sol
+++ Router.sol
@@ -2,6 +2,27 @@
 
 pragma solidity >=0.5.0;
 
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with GSN meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
 interface IAKITAFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
@@ -250,6 +271,69 @@ interface IAKITAPair {
     function initialize(address, address) external;
 }
 
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
 
 // File contracts/libraries/SafeMath.sol
 
@@ -293,7 +377,7 @@ library AKITALibrary {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'e9206081f9a542c769bd3a99e0b93beefbd21751118cdcdb8cdb908226403d2b' // init code hash
+                hex'6442a8f13c0d032c191e04f620810957afbc21cb14765e3e7928d8454a8089ab' // init code hash
             ))));
     }
 
@@ -438,7 +522,7 @@ interface IBlast{
 pragma solidity =0.6.6;
 
 
-contract AKITARouter is IAKITARouter02 {
+contract AKITARouter is IAKITARouter02,Ownable {
     using SafeMath for uint;
 
     address public immutable override factory;
@@ -450,11 +534,10 @@ contract AKITARouter is IAKITARouter02 {
         _;
     }
 
-    constructor(address _factory, address _WETH, address gov) public {
+    constructor(address _factory, address _WETH) public {
         factory = _factory;
         WETH = _WETH;
         IBlast(BLAST).configureClaimableGas() ;
-		IBlast(BLAST).configureGovernor(gov);
     }
 
     receive() external payable {
@@ -877,7 +960,7 @@ contract AKITARouter is IAKITARouter02 {
     }
 
     //It claims the gasfee Yield accured by this contract 
-    function claimAllGas(address recipient) external {
+    function claimAllGas(address recipient) external onlyOwner {
 	    // This function is public meaning anyone can claim the gas
 		IBlast(BLAST).claimAllGas(address(this), recipient);
     }
